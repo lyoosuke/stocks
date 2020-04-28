eaa775fd1d60e4c1bb5ec8f1323d886b030712c9
lyoosuke <57707923+lyoosuke@users.noreply.github.com>
2020-04-28 21:56:31 +0900

    Update CNN_pred.py

--- CNN_pred.py
+++ CNN_pred.py
@@ -1,12 +1,19 @@
-import tensorflow as tf 
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import Adam
 #sess = tf.compat.v1.Session()
+kobetu = "9613"
 image = []
 for i in range(30, 2432):
-    png = tf.io.read_file('9613_average_30_' + str(i) + '.png')
+    png = tf.io.read_file(kobetu + '_average_10_' + str(i) + '.png')
     image.append(tf.image.decode_png(png, channels=1, dtype=tf.uint8))
 image_float = tf.cast(image,dtype=tf.float32)
 print(image_float)
-image_reshape = tf.reshape(image_float, [-1, 307200])
+image_reshape = tf.reshape(image_float, [-1, 3072])
 print(image_reshape)
 
 
@@ -15,12 +22,20 @@ X_test = image_reshape[2175:2403]
 X_train /= 255
 X_test /= 255
 
-
-
-Y = pd.read_csv('modified_answer_9613.csv', encoding="shift-jis")
+stock_data = pd.read_csv(kobetu + "_tech4.csv", encoding="shift-jis")
+Y = []
+answers = []
+for i in range(30,2432):
+    Y.append((float(stock_data.loc[i, ['調整後終値']])*100 / float(stock_data.loc[i-1, ['調整後終値']]))-100)
+for i in range(30,2432):
+    if Y[i-30]>0:
+        answers.append(1)
+    else:
+        answers.append(0)
+#Y = pd.read_csv('modified_answer_9613.csv', encoding="shift-jis")
 print(Y)
-y_train = Y[0:2175]
-y_test = Y[2175:2403]
+y_train = answers[0:2175]
+y_test = answers[2175:2403]
 y_train = np_utils.to_categorical(y_train, 2)
 y_test = np_utils.to_categorical(y_test, 2)
 print(y_train)
@@ -30,7 +45,7 @@ print(y_test)
 
 #モデルの構造を定義
 model = Sequential()
-model.add(Dense(512, input_shape=(307200,)))
+model.add(Dense(512, input_shape=(3072,)))
 model.add(Activation('relu'))
 model.add(Dropout(0.1))
 
@@ -59,7 +74,3 @@ print('accuracy=', score[1]/5)
 
 img_pred = model.predict(X_test, steps=5)
 print(img_pred)
-
-
-
-
